bd803030e41ff257e4e154d77dada6fafec6be08
Mubashar Javed <mubasharbravo302@gmail.com>
2019-10-27 01:38:24 +0500

    second commit

--- basic_app/views.py
+++ basic_app/views.py
@@ -21,31 +21,3 @@ def register(request):
         return redirect('http://127.0.0.1:8000/home')
 
     return render(request, 'basic_app/registration.html', {'form': form})
-
-    # if request.method == 'POST':
-    #     user_form = UserForm(data=request.POST)
-    #     profile_form = UserProfileInfoForm(data=request.POST)
-
-    #     if user_form.is_valid() and profile_form.is_valid():
-
-    #         user = user_form.save()
-
-    #         user.set_password(user.password)
-    #         user.save()
-
-    #         profile = profile_form.save(commit=False)
-    #         profile.user = user
-
-    #         if 'profile_pic' in request.FILES:
-    #             profile.profile_pic = request.FILES['profile_pic']
-
-    #         profile.save()
-
-    #         registered = True
-
-    #     else:
-    #         print(user_form.errors, profile_form.errors)
-
-    # else:
-    #     user_form = UserForm()
-    #     profile_form = UserProfileInfoForm()
